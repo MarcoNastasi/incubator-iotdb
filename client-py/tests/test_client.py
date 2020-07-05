d4b72c8395890d7aabed8be980d22306424840f6
Tarnus <tarnus88@gmail.com>
2020-07-06 01:43:28 +0200

    Made adjustments

--- client-py/tests/test_client.py
+++ client-py/tests/test_client.py
@@ -20,11 +20,13 @@ import unittest
 import subprocess
 import os
 
+sys.path.append("../../service-rpc/target/generated-sources-python")
+
 class TestBasic(unittest.TestCase):
 
     def test_functionality(self):
         path = os.path.dirname(os.path.realpath(__file__))
-        self.assertEqual(subprocess.run(['python3', f'{path}/../src/client_example.py']).returncode, 0)
+        self.assertEqual(subprocess.run(['python3', f'client_example.py']).returncode, 0)
 
 
 if __name__ == '__main__':
